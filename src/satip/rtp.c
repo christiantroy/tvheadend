82f506f5f2b3a27d692ff155ca5ff189f2bd8764
Alan Marchesan <justcavalli@gmail.com>
2015-05-01 07:55:53 +0200

    android: include the required headers in 'src/satip/rtp.c' file to fix "implicit declaration of function 'sendto'" compile error

--- src/satip/rtp.c
+++ src/satip/rtp.c
@@ -17,6 +17,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <signal.h>
 #include <ctype.h>
 #include "tvheadend.h"
