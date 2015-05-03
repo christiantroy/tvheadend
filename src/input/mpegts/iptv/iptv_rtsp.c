74e0e9af657e782c74e60ada1051b7abae8153b3
Alan Marchesan <justcavalli@gmail.com>
2015-05-03 13:28:19 +0200

    android: include the required headers in 'src/input/mpegts/iptv/iptv_rtsp.c' file to fix "implicit declaration of function 'sendto'" compile error

--- src/input/mpegts/iptv/iptv_rtsp.c
+++ src/input/mpegts/iptv/iptv_rtsp.c
@@ -17,6 +17,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <sys/socket.h>
+#include <sys/types.h>
 #include "tvheadend.h"
 #include "iptv_private.h"
 #include "http.h"
